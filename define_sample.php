cd2143b53deec1d7f3ceb3e164b31187eefcf4b5
Mohamed Goush <69215006+MohamedGoush@users.noreply.github.com>
2022-01-14 02:59:22 +0530

    define example in php

---
+++ define_sample.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * It is used to put the values in constance.
+ * 
+ * in future any one can change the folder name only change the url in defined field only.
+ * 
+ */
+
+define("Home","localhost");    // in future change the localhost into any url then change one side only.
+
+
+define("Greetings","Hello Mohamed How are You?");
+
+echo constant("Greetings");
+
+
+?>
