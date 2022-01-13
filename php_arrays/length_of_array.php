51744f8d74516b6cdd8e7f57fe85199ae3e12a99
Mohamed Goush <69215006+MohamedGoush@users.noreply.github.com>
2022-01-14 03:00:00 +0530

    php array examples

---
+++ php_arrays/length_of_array.php
@@ -0,0 +1,11 @@
+<?php
+
+$cars = array("Volvo","BMW","Maruthi");
+
+echo $cars[0]. "<br>";
+echo $cars[2]. "<br>";
+echo $cars[1]. "<br>";
+
+echo count($cars);
+
+?>
---
+++ php_arrays/static_array.php
@@ -0,0 +1,9 @@
+<?php
+
+$cars = array("Volvo","BMW","Maruthi");
+
+echo $cars[0]. "<br>";
+echo $cars[2]. "<br>";
+echo $cars[1]. "<br>";
+
+?>
